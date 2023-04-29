f6cea3169489266035f86597564af1c6d65fa8f2
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-04-29 15:17:01 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import apiKey from "./config.js"
+
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-input')
 const searchResultsEl = document.getElementById('search-results')
@@ -12,7 +14,7 @@ function getMovieData(e) {
     e.preventDefault()
     const searchQuery = searchInput.value 
     console.log(searchQuery)
-    fetch(`https://www.omdbapi.com/?apikey=afd50df&s=${searchQuery}`)
+    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`)
         .then (res => res.json())
         .then (data => {
             console.log(data)
