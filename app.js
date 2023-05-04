a77abe358c71203bc31eda7e220dcada234c8b51
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-05-04 18:18:40 +0700

    add styling for movie cards and update app.js

--- app.js
+++ app.js
@@ -28,7 +28,24 @@ function renderResults(array) {
     searchResultsEl.innerHTML = ''
     array.forEach(movie => {
         searchResultsEl.innerHTML += `
-            <p>${movie.Title}</p>
+            <div class="card">
+                    <div class="movie-poster">
+                        <img id="movie-poster-img" src="${movie.Poster}" alt="">
+                    </div>
+                    <div class="movie-info">
+                        <div class="movie-info__header">
+                            <p class="movie-title">${movie.Title}</p>
+                            <div class="ratings-star-container"><img id="ratings-star" src="./star.png" alt="yellow star"></div>
+                            <p class="movie-rating">${movie.imdbRating}</p>
+                        </div>
+                        <div class="movie-info__metadata">
+                            <p class="movie-runtime spacing">${movie.Runtime}</p>
+                            <p class="movie-genre spacing">${movie.Genre}</p>
+                            <button id="watchlist-btn"><i class="fa-solid fa-circle-plus"></i> Watchlist</button>
+                        </div>
+                        <p class="movie-info__plot">${movie.Plot}</p>
+                    </div>
+                </div>
         `
         
     });
